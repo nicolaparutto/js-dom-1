f6e9e760bf983118d2f2288a9439a33b03253c8c
nicolaparutto <nicolaparutto2003@gmail.com>
2024-11-21 15:15:03 +0100

    feat(bonus): add bonus exercise

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const yellowLamp = document.querySelector('.yellow-lamp');
 button.addEventListener('click', () => {
    whiteLamp.classList.toggle('d-none');
    yellowLamp.classList.toggle('d-block');
+   button.innerHTML = whiteLamp.classList.contains('d-none') ? 'Spegni la lampadina' : 'Accendi la lampadina'
 });
 
 
